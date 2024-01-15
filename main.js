e3802549cd77e1082e330de2955cee43096f7024
kyaukyuai <y.kakui@gmail.com>
2024-01-15 16:33:41 +0900

    Add files via your-dev-team

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-document.getElementById('startPauseBtn').addEventListener('click', handleStartPauseClick);
+    document.getElementById('startPauseBtn').addEventListener('click', handleStartPauseClick);
 document.getElementById('resetBtn').addEventListener('click', handleResetClick);
+
+let taskInput = document.getElementById('taskInput');
+let activeTask = document.getElementById('activeTask');
 
 function handleStartPauseClick() {
   if (this.textContent === 'Start') {
     this.textContent = 'Pause';
+    activeTask.textContent = 'Active Task: ' + taskInput.value;
     startTimer();
   } else {
     this.textContent = 'Start';
@@ -14,4 +18,7 @@ function handleStartPauseClick() {
 function handleResetClick() {
   document.getElementById('startPauseBtn').textContent = 'Start';
   resetTimer();
+  activeTask.textContent = '';
 }
+
+    
